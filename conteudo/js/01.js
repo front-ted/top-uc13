1e7566d091df9440ce50962bcd40382e893bfbbe
FabianaRossatto <fmrossatto@senacrs.com.br>
2024-08-09 11:57:49 -0300

    conteudo

---
+++ conteudo/js/01.js
@@ -0,0 +1,20 @@
+$('.flip-card').click(function(){
+    if($(this).hasClass('active')){$(this).removeClass('active')}else{$(this).addClass('active')}    
+})
+$(".menu label.menu-toggle").click(function(){
+    $(".bg-fundo").toggleClass("mod");
+    $('body').toggleClass('overflow');
+    $('.menu').toggleClass('fade-check');
+});
+
+$(".bg-fundo").click(function(){
+    $(this).removeClass("mod");
+    $('body').removeClass('overflow');
+    $('.menu').removeClass('fade-check');
+});
+
+$(".fundo-item").click(function(){
+    $(".bg-fundo").removeClass("mod");
+    $('body').removeClass('overflow');
+    $('.menu').removeClass('fade-check');
+});
---
+++ conteudo/js/carousel.js
@@ -0,0 +1,15 @@
+
+var splide = new Splide('.splide', {
+  type: 'slide',
+  perPage: 2,
+  trimSpace: false,
+  focus: 'center'
+});
+
+splide.mount();
+
+
+
+
+
+
